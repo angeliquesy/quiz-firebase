703bc0d18605b2150f33fc6369062c2f7e806718
angeliquesy <anzhelika.sych@gmail.com>
2021-09-27 16:29:51 +0700

    ways of adding a component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Car from './Car/Car'
 
 class App extends Component {
   render() {
     const divStyle = {
       textAlign: 'center',
-      color: 'red'
     }
     return (
       <div className="App" style={divStyle}>
-        <h1 style={{fontSize: '244px'}}>Hello world!</h1>
+        <h1>Hello world!</h1>
+        <Car />
       </div>
     );
-
-    // return React.createElement(
-    //   'div',
-    //   {
-    //     className: 'App'
-    //   },
-    //   React.createElement(
-    //     'h1',
-    //     null,
-    //     'Hello world!'
-    //   )
-    // )
   }
 }
 
---
+++ src/Car/Car.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import App from "../App";
+
+// function Car() {
+//   return (
+//     <h2>This is a Car component</h2>
+//   )
+// }
+
+// const car = () => {
+//   return (
+//      <h2>This is a Car component</h2>
+//   )
+// }
+
+// const car = () => <h2>This is a Car component</h2>
+
+// const car = () => (
+//   <div>
+//     <h2>This is a Car component</h2>
+//     <strong>test</strong>
+//   </div>
+// )
+
+export default () => (
+  <div>
+    <h2>This is a Car component</h2>
+    <strong>test</strong>
+  </div>
+);
