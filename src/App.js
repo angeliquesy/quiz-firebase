df228a58f9276d377cfdd99547e5064e91ae7bb5
angeliquesy <anzhelika.sych@gmail.com>
2021-09-28 20:13:31 +0700

    style: Radium

--- src/App.js
+++ src/App.js
@@ -40,6 +40,13 @@ class App extends Component {
     })
   }
 
+  deleteHandler(index) {
+    const cars = this.state.cars.concat()
+    cars.splice(index, 1)
+
+    this.setState({cars})
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center',
@@ -54,6 +61,7 @@ class App extends Component {
             key={index}
             name={car.name}
             year={car.year}
+            onDelete={this.deleteHandler.bind(this, index)}
             onChangeName={event => this.onChangeName(event.target.value, index)}
             // лучший способ (оптимизация): onChangeTitle={this.changeTitleHandler.bind(this, car.name)}
           />
