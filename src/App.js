c07118edc0aa09809d75b65e6b2111439b2a33af
angeliquesy <anzhelika.sych@gmail.com>
2021-10-04 10:57:26 +0700

    adding menu

--- src/App.js
+++ src/App.js
@@ -1,117 +1,13 @@
-import React, {Component} from 'react';
-import './App.css';
-import Car from './Car/Car'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-import Counter from "./Counter/Counter";
-
-export const ClickedContext = React.createContext(false)
+import React, { Component } from 'react';
+import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props) // т.к Component имеет свои пропс, нужно их наследовать
-
-    this.state = {
-      clicked: false,
-      cars: [
-        {
-          name: 'Ford',
-          year: 2018
-        },
-        {
-          name: 'Audi',
-          year: 2016
-        },
-        {
-          name: 'Mazda',
-          year: 2010
-        },
-      ],
-      pageTitle: 'React components',
-      showCars: false
-    }
-  }
-
-  onChangeName = (name, index) => {
-    const car = this.state.cars[index]
-    car.name = name
-    const cars = [...this.state.cars]
-    // const cars = this.state.cars.concat() // клонирование массива - старый способ
-    cars[index] = car
-    this.setState({cars}) // = cars: cars
-
-    // когда меняем состояние, должны создавать клон и новый стейт, т.к. не можем напрямую менять состояние
-  }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars
-    })
-  }
-
-  deleteHandler(index) {
-    const cars = this.state.cars.concat()
-    cars.splice(index, 1)
-
-    this.setState({cars})
-  }
-
-  UNSAFE_componentWillMount() {
-    console.log('App componentWillMount')
-  }
-
-  componentDidMount() {
-    console.log('App componentDidMount')
-  }
-
   render() {
-    console.log('App render')
-
-    const divStyle = {
-      textAlign: 'center',
-    }
-
-    let cars = null
-
-    if (this.state.showCars) {
-      cars = this.state.cars.map((car, index) => {
-        return (
-          <ErrorBoundary key={index}>
-            <Car
-              name={car.name}
-              year={car.year}
-              index={index}
-              onDelete={this.deleteHandler.bind(this, index)}
-              onChangeName={event => this.onChangeName(event.target.value, index)}
-              // лучший способ (оптимизация): onChangeTitle={this.changeTitleHandler.bind(this, car.name)}
-            />
-          </ErrorBoundary>
-        )
-      })
-    }
-
     return (
-      <div className='App' style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
-
-        <ClickedContext.Provider value={this.state.clicked}>
-          <Counter/>
-        </ClickedContext.Provider>
-
-
-        <hr/>
-
-        <button onClick={this.toggleCarsHandler}>
-          ToggleCars
-        </button>
-
-        <button onClick={() => this.setState({clicked: true})}>
-          Change click
-        </button>
-
-        {cars}
-
-      </div>
+      <Layout>
+        <Quiz/>
+      </Layout>
     );
   }
 }
