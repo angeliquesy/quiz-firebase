c30307d4851a76b7ecc91ca2ecd6573d2e1852c6
angeliquesy <anzhelika.sych@gmail.com>
2021-12-18 16:05:54 +0300

    fixing a typo in about

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -5,7 +5,7 @@ const About = () => (
   <div className={classes.About}>
     <div>
       <p>This website is&nbsp;built on&nbsp;React, hosted, and served by&nbsp;Firestore. All user data goes to&nbsp;Firestore Database.</p>
-      <p><em>"&laquo;Who wants to&nbsp;be&nbsp;a&nbsp;millionaire&raquo;</em> and <em>&laquo;Frontend IT&nbsp;quiz&raquo;</em> games
+      <p><em>&laquo;Who wants to&nbsp;be&nbsp;a&nbsp;millionaire&raquo;</em> and <em>&laquo;Frontend IT&nbsp;quiz&raquo;</em> games
         each time give a&nbsp;different pack of&nbsp;10&nbsp;random questions.</p>
       <p>To&nbsp;<strong>create your own quiz</strong> you should sign&nbsp;up. There is&nbsp;no&nbsp;email verification,
         input any email and password, let the browser remember it&nbsp;for you to&nbsp;be&nbsp;able to&nbsp;sign in&nbsp;again.</p>
